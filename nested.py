81b25bf4a0c777d569b37a8b6290adc72154b514
Mudeef <96822029+Mudeef@users.noreply.github.com>
2022-08-09 15:16:22 +0530

    nested

---
+++ nested.py
@@ -0,0 +1,25 @@
+# nested_distionary={
+#     "one":
+#     {
+#     "name":"John",
+#     "age":30,
+#     "city":"New york"
+# },
+#  "two":
+#     {
+#     "name":"Jane",
+#     "age":25,
+#     "city":"paris"
+# },
+#  "three":
+#     {
+#     "name":"Bobby",
+#     "age":20,
+#     "city":"London"
+# }
+# }
+# print(nested_distionary)
+# print(nested_distionary["three"]["name"])
+#
+# print("======================================================")
+

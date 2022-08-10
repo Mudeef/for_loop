ac00f5bfedcb405fd8878e06d35f03ac4b46617c
Mudeef <96822029+Mudeef@users.noreply.github.com>
2022-08-10 14:06:57 +0530

    upload

--- for_loop_advanced.py
+++ for_loop_advanced.py
@@ -1,14 +1,3 @@
-# mul=1
-# for i in range(1,11):
-#     mul*=i
-# print(mul)
-
-num=12345
-sum=0
-for i in range(len(str(num))):  #convert string and calculate length
-    temp=num%10   #get last value
-    num=num//10   #remove last value
-    print(num)
-    sum+=temp    #
-print(sum)
-#
+num=2
+for i in range(1,11):
+    print(i,"x",num,"=",i*num)
